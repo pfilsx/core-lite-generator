b6b87db422d575194d679d2d3fa1fc25e9fb0d51
Pavel <honorhelp@gmail.com>
2017-11-27 15:49:04 +0300

    fixed js asset

--- assets/crl.core-gen.main.js
+++ assets/crl.core-gen.main.js
@@ -1,24 +1,27 @@
+var table_name = $('[data-attribute=table_name]');
+var model_name = $('[data-attribute=model_name]');
+
 $('#table_name').on('change', function(){
    var obj = $(this);
    var infoBlock = $('.info-block');
    if (obj.val().trim().length < 1){
        infoBlock.find('input').prop('disabled', true);
-       $('[data-field=table_name]').val('');
-       $('[data-field=model_name]').val('');
+       table_name.val('');
+       model_name.val('');
        return;
    }
    var tableName = obj.val().trim();
-    $('[data-field=table_name]').val(tableName).blur();
+   table_name.val(tableName).blur();
     var modelClass = '';
     $.each(tableName.split('_'), function() {
         if(this.length>0)
             modelClass+=this.substring(0,1).toUpperCase()+this.substring(1);
     });
-    $('[data-field=model_name]').val(modelClass).blur();
+    model_name.val(modelClass).blur();
     infoBlock.find('input').prop('disabled', false);
 });
 
-$('[data-field=model_name]').on('blur', function(){
+model_name.on('blur', function(){
     if ($(this).val().trim().length > 0){
         $('input[type=submit]').prop('disabled', false);
     } else {
